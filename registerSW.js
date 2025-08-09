8e93b1a52c4bf535af989044b37ac57b82546834
Esai-Keshav <Esai-Keshav@users.noreply.github.com>
2025-08-09 17:58:36 +0000

    deploy: c25930adbaa0e223a76a2c3c279fc1acafa6021c

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/annamalai-university-cgpa/sw.js', { scope: '/annamalai-university-cgpa/' })})}
